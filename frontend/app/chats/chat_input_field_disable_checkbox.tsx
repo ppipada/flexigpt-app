958bc24f44641675e3bbc920aad4c9ad7b449109
Pankaj Pipada <ppipada@gmail.com>
2025-04-21 15:49:28 +0530

    disable prev messages tooltip

--- frontend/app/chats/chat_input_field_disable_checkbox.tsx
+++ frontend/app/chats/chat_input_field_disable_checkbox.tsx
@@ -9,18 +9,20 @@ export default function DisablePreviousMessagesCheckbox(props: {
 
 	return (
 		<div className="flex w-full justify-center">
-			<label className="flex ml-4 space-x-2 text-neutral-400 overflow-hidden" title="Disable previous messages">
-				<input
-					type="checkbox"
-					checked={disablePreviousMessages}
-					onChange={e => {
-						setDisablePreviousMessages(e.target.checked);
-					}}
-					className="checkbox checkbox-xs rounded-full"
-					spellCheck="false"
-				/>
-				<span className="text-xs text-nowrap">Disable previous messages</span>
-			</label>
+			<div className="tooltip" data-tip="Send only this message to AI">
+				<label className="flex ml-4 space-x-2 text-neutral-400 overflow-hidden">
+					<input
+						type="checkbox"
+						checked={disablePreviousMessages}
+						onChange={e => {
+							setDisablePreviousMessages(e.target.checked);
+						}}
+						className="checkbox checkbox-xs rounded-full"
+						spellCheck="false"
+					/>
+					<span className="text-xs text-nowrap">Ignore chat history</span>
+				</label>
+			</div>
 		</div>
 	);
 }
