8c6b092441000f4dd860ce63c6fc03d39c4fcf10
ppipada <ppipada@gmail.com>
2025-06-18 13:25:55 +0530

    gemini flash lite consts

--- pkg/model/consts/google.go
+++ pkg/model/consts/google.go
@@ -7,23 +7,27 @@ import (
 const ProviderNameGoogle spec.ProviderName = "google"
 
 const (
-	Gemini25Pro   spec.ModelName = "gemini-2.5-pro-preview-05-06"
-	Gemini25Flash spec.ModelName = "gemini-2.5-flash-preview-05-20"
+	Gemini25Pro       spec.ModelName = "gemini-2.5-pro"
+	Gemini25Flash     spec.ModelName = "gemini-2.5-flash"
+	Gemini25FlashLite spec.ModelName = "gemini-2.5-flash-lite-preview-06-17"
 )
 
 const (
-	DisplayNameGemini25Pro   spec.ModelDisplayName = "Google Gemini 2.5 Pro"
-	DisplayNameGemini25Flash spec.ModelDisplayName = "Google Gemini 2.5 Flash"
+	DisplayNameGemini25Pro       spec.ModelDisplayName = "Google Gemini 2.5 Pro"
+	DisplayNameGemini25Flash     spec.ModelDisplayName = "Google Gemini 2.5 Flash"
+	DisplayNameGemini25FlashLite spec.ModelDisplayName = "Google Gemini 2.5 Flash Lite"
 )
 
 const (
-	ShortCommandGemini25Pro   spec.ModelShortCommand = "gemini25Pro"
-	ShortCommandGemini25Flash spec.ModelShortCommand = "gemini25Flash"
+	ShortCommandGemini25Pro       spec.ModelShortCommand = "gemini25Pro"
+	ShortCommandGemini25Flash     spec.ModelShortCommand = "gemini25Flash"
+	ShortCommandGemini25FlashLite spec.ModelShortCommand = "gemini25FlashLite"
 )
 
 const (
-	ModelPresetIDGemini25Pro   spec.ModelPresetID = "gemini25Pro"
-	ModelPresetIDGemini25Flash spec.ModelPresetID = "gemini25Flash"
+	ModelPresetIDGemini25Pro       spec.ModelPresetID = "gemini25Pro"
+	ModelPresetIDGemini25Flash     spec.ModelPresetID = "gemini25Flash"
+	ModelPresetIDGemini25FlashLite spec.ModelPresetID = "gemini25FlashLite"
 )
 
 var GoogleModelPresets = map[spec.ModelPresetID]spec.ModelPreset{
@@ -46,6 +50,20 @@ var GoogleModelPresets = map[spec.ModelPresetID]spec.ModelPreset{
 		Name:         Gemini25Flash,
 		DisplayName:  DisplayNameGemini25Flash,
 		ShortCommand: ShortCommandGemini25Flash,
+		IsEnabled:    true,
+
+		Stream:          BoolPtr(true),
+		MaxPromptLength: IntPtr(32768),
+		MaxOutputLength: IntPtr(32768),
+		Temperature:     Float64Ptr(0.1),
+		SystemPrompt:    StringPtr(""),
+		Timeout:         IntPtr(120),
+	},
+	ModelPresetIDGemini25FlashLite: {
+		ID:           ModelPresetIDGemini25FlashLite,
+		Name:         Gemini25FlashLite,
+		DisplayName:  DisplayNameGemini25FlashLite,
+		ShortCommand: ShortCommandGemini25FlashLite,
 		IsEnabled:    true,
 
 		Stream:          BoolPtr(true),
